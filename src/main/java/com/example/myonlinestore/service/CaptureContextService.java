b5ebf79ca0589f724b2969b7784e34d7d9dfcacd
rajvpate <rajvpate@visa.com>
2025-08-06 11:50:42 -0700

    Fixed the package import

--- src/main/java/com/example/myonlinestore/service/CaptureContextService.java
+++ src/main/java/com/example/myonlinestore/service/CaptureContextService.java
@@ -2,7 +2,7 @@ package com.example.myonlinestore.service;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import com.example.model.myonlinestore.RequestData;
+import com.example.myonlinestore.model.RequestData;
 
 import java.util.*;
 
