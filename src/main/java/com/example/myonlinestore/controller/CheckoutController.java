c4689fc629dd62f733b235681ca3df9bc78804e2
rajvpate <rajvpate@visa.com>
2025-08-06 17:00:34 -0700

    Add permissions policy header

--- src/main/java/com/example/myonlinestore/controller/CheckoutController.java
+++ src/main/java/com/example/myonlinestore/controller/CheckoutController.java
@@ -74,6 +74,9 @@ public class CheckoutController {
     public String showCheckout(Model model) {
 
         try {
+             // Add Permissions Policy header
+        response.setHeader("Permissions-Policy", "payment=*");
+        
             /* CyberSource round-trip ------------------------------------------------ */
             String jwt = captureContextService.getCaptureContext(requestData);
 
